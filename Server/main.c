d848ac4762ddc8c385b915cadf4855958040756f
mcany <83024513+hartigan8@users.noreply.github.com>
2022-12-18 14:25:44 +0300

    Initial Commit

---
+++ Server/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Server
+//
+//  Created by Mustafa Can YILMAZ on 18.12.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
